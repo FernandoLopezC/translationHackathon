4f7abbd2a4f2aa9b8f0ed9d9daba232e9c722231
FernandoLopezC <lopez.castrof28@gmail.com>
2024-09-24 16:23:48 +0100

    starting batch

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+import logging
+import os
+import sys
+from flask import Flask
+
+app = Flask(__name__)
+
+
+logging.basicConfig(filename='logs/translationLogs.log', level=logging.ERROR,
+                    format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+
+
+from app import routes
+
---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from app import app
+from flask import render_template, request
+from google.cloud import translate_v2 as translate
+
+# Initialize the Google Cloud Translation API client
+translate_client = translate.Client("AIzaSyBmML2nQlobtJh7C3ZcCNxIW6Wr1aWfcyM")
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    translated_text = None
+    source_language = None
+    target_language = None
+    if request.method == 'POST':
+        text_to_translate = request.form['text']
+        target_language = request.form['target_language']
+        # Detect language and translate
+        translation = translate_client.translate(text_to_translate, target_language=target_language)
+        translated_text = translation['translatedText']
+        source_language = translation['detectedSourceLanguage']
+
+    return render_template('index.html', translated_text=translated_text,
+                           source_language=source_language, target_language=target_language)
